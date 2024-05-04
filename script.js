6fa37d23647a9b0e095e423541f258e0935b27ce
Krystle Capuano <krystlecapuano@gmail.com>
2024-05-03 21:10:54 -0400

    "Functions"

--- script.js
+++ script.js
@@ -7,4 +7,29 @@ const name = form.elements['name'];
 const email = form.elements['email'];
 const number = form.elements['number'];
 
-form.addEventListener('submit', validate)
+form.addEventListener('submit', validate);
+
+
+function validate(e) {
+const nameVal = validateName();
+if (nameVal === false) {
+e.returnValue = false;
+return false;
+}
+
+const emailVal = validateEmail();
+if (emailVal === false) {
+e.returnValue = false;
+return false;
+}
+
+const numberVal = validateNumber();
+if (numberVal === false) {
+e.returnValue = false;
+return false;
+}
+
+
+
+
+}
