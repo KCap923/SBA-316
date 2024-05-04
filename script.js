3d59a55cfe5d821fcd639e8e5e093d3b8edc0551
Krystle Capuano <krystlecapuano@gmail.com>
2024-05-03 20:42:42 -0400

    "Form Validation"

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // alert("Cool Chicks Rise Up!!!")
 
+// Form Validation *************************************************
 
-const waitlistForm = document.querySelector("[waitlist-form]") 
+const form = document.getElementById ('form');
+const name = form.elements['name'];
+const email = form.elements['email'];
+const number = form.elements['number'];
+
+form.addEventListener('submit', validate)
